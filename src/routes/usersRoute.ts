076bbfed2113aef93e4b2a315c9f38c29a5ddec5
oleesir <oleesir@gmail.com>
2022-04-29 18:37:01 +0100

    user can update their profile

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getAllUsers, getUser, updateUser } from "../controllers/users.controller";
+import { getAllUsers, getUser, updateUser, updateProfile } from "../controllers/users.controller";
 import { isAdmin, isAuth } from "../middleware/authorization.middleware";
-import { validateUpdatedUser } from "../middleware/validation.middleware";
+import { validateUpdatedUser, validateUserProfile } from "../middleware/validation.middleware";
 import asyncHandler from "../middleware/asyncErrorHandler.middleware";
 
 const router: Router = Router();
 
+router.put("/:_id", isAuth, validateUserProfile, asyncHandler(updateProfile));
 router.get("/", isAuth, isAdmin, asyncHandler(getAllUsers));
 router.get("/:_id", isAuth, isAdmin, asyncHandler(getUser));
 router.put("/:_id", isAuth, isAdmin, validateUpdatedUser, asyncHandler(updateUser));
