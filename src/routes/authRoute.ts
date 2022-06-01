ae214178eaa72a3e419fb9c599897435958c0b85
oleesir <oleesir@gmail.com>
2022-06-01 13:36:59 +0100

    create log out user endpoint

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { registerUser, loginUser, loggedInUser, googleOAuth } from "../controllers/auth.controller";
+import { registerUser, loginUser, loggedInUser, googleOAuth, logoutUser } from "../controllers/auth.controller";
 import asyncHandler from "../middleware/asyncErrorHandler.middleware";
 import { validateRegisteredUser, validateLoginUser } from "../middleware/validation.middleware";
 
 const router: Router = Router();
 
-router.get("/sessions/oauth/google", googleOAuth);
+router.get("/sessions/oauth/google", asyncHandler(googleOAuth));
 router.post("/signup", validateRegisteredUser, asyncHandler(registerUser));
 router.post("/login", validateLoginUser, asyncHandler(loginUser));
+router.get("/logout", asyncHandler(logoutUser));
 router.get("/loggedin", asyncHandler(loggedInUser));
 
 export default router;
