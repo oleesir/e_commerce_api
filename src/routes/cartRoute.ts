630217c55b317144a058f3b05708f57f28d203a6
oleesir <oleesir@gmail.com>
2022-07-15 17:56:13 +0100

    remove items in cart

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addItemToCart, reduceItemsInCart } from "../controllers/cart.controller";
+import { addItemToCart, reduceItemsInCart, removeItemsInCart } from "../controllers/cart.controller";
 import asyncErrorHandler from "../middleware/asyncErrorHandler.middleware";
 import { roles } from "../utils/constants";
 import { authorizedRole, isAuth } from "../middleware/authorization.middleware";
@@ -12,6 +12,13 @@ router.post(
 	isAuth,
 	authorizedRole([roles.ADMIN, roles.SELLER, roles.CUSTOMER]),
 	asyncErrorHandler(reduceItemsInCart),
+);
+
+router.delete(
+	"/remove/:productId",
+	isAuth,
+	authorizedRole([roles.ADMIN, roles.SELLER, roles.CUSTOMER]),
+	asyncErrorHandler(removeItemsInCart),
 );
 
 export default router;
