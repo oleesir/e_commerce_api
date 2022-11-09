76f652f21ee1874f082ce26d5875626502bd5aac
oleesir <oleesir@gmail.com>
2022-11-09 15:42:53 -0500

    create order

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,60 @@
+import {Request, Response} from "express";
+import Cart from "../database/models/cartModel";
+import User from "../database/models/userModel";
+import Order from "../database/models/orderModel";
+
+/**
+ * create order
+ * @method createOrder
+ * @memberof orderController
+ * @param {object} req
+ * @param {object} res
+ * @returns {(function|object)} Function next() or JSON object
+ */
+export const createOrder = async (req: Request, res: Response) => {
+    const {
+        cartId,
+        address,
+        phoneNumber,
+        country,
+        paymentMethod,
+      } = req.body;
+    const {_id: userId} = (<any>req).user;
+
+    let userCart = await Cart.findOne({userId,cartId});
+    let user = await User.findById({_id:userId});
+
+    if (!userCart) {
+        return res.status(404).json({status: "failed", message: "No cart found"});
+    }
+
+const subTotal = userCart?.totalPrice
+
+    //calculate total price with tax and shipping
+    const totalCost =(tax:number,shipping:number)=>{
+        const taxInCents = tax * 100;
+        const shippingCostInCents = shipping * 100;
+
+       return taxInCents + shippingCostInCents + subTotal;
+    }
+
+    const order = new Order({
+        userId,
+        cartId,
+        address:user?.address || address,
+        email:user?.email,
+        country,
+        phoneNumber,
+        paymentMethod,
+        taxPrice:parseInt(process.env.TAX_PRICE as string, 10) * 100,
+        shippingPrice:parseInt(process.env.SHIPPING_PRICE as string, 10) * 100,
+        totalPrice:totalCost(parseInt(process.env.TAX_PRICE as string, 10),parseInt(process.env.SHIPPING_PRICE as string, 10)),
+
+    })
+
+
+    const data = await order.save();
+    return res.status(201).json({status: "success", data});
+
+
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRoute from "./authRoute";
 import cartRoute from "./cartRoute";
 import usersRoute from "./usersRoute";
+import orderRoute from "./orderRoute";
 import productRoute from "./productRoute";
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 router.use("/auth", authRoute);
 router.use("/users", usersRoute);
 router.use("/carts", cartRoute);
+router.use("/orders", orderRoute);
 router.use("/products", productRoute);
 
 export default router;
